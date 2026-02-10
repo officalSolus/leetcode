13af52470dae32cd315c7819e3899ffaae14dc33
officalSolus <work.solus.dev@gmail.com>
2026-02-10 07:30:04 +0530

    new

---
+++ programs/test.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        seen = set()
+        n = 0
+        left = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left+=1
+            
+            seen.add(s[right])
+            n = max(n, right-left+1)
+        return n
+            
